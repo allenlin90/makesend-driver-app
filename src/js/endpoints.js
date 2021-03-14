6e81781a6aa27376f9f17f74978d49fd31313f89
allenlin90 <ht016746@gmail.com>
2021-03-14 11:55:56 +0700

    change to correct endpoint

--- src/js/endpoints.js
+++ src/js/endpoints.js
@@ -1,4 +1,4 @@
-const host = 'https://makesend-driver.herokuapp.com/'//window.location.hostname === '127.0.0.1' || 'localhost' ? 'http://localhost:8080' : '';
+const host = 'https://makesend-driver.herokuapp.com'//window.location.hostname === '127.0.0.1' || 'localhost' ? 'http://localhost:8080' : '';
 const endpoints = {
     host,
     checkToken: 'https://api.airportels.ninja/api/msd/authPing'
